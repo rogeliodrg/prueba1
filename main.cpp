1f0a6abd9821184238ae7ae316aaf832d064909d
Rogelio Daniel Ramirez Gonzalez <purochivas_chino@hotmail.com>
2015-05-18 07:57:11 -0500

    se agregaron archivos al proyecto

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <cv.hpp>
+#include <cxcore.hpp>
+#include <highgui.h>
+
+int main(int argc, char** argv)
+{
+    cv::Mat frame;
+    int c;
+    CvCapture *capture = cvCaptureFromCAM(0);
+    for(;;)
+    {
+        frame = cvQueryFrame(capture);
+        cv::imshow("OpenCV",frame);
+        c = cv::waitKey(10);
+        if(c==27)
+            break;
+    }
+    return 0;
+}
